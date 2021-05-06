e58098e8bf8aa30cb6685bf92308547b631fda63
ZhaoXuan <2373952157@qq.com>
2021-05-06 14:50:32 +0800

    zhao xuan html

---
+++ src/main/java/com/example/team2021/Controller/backController.java
@@ -0,0 +1,14 @@
+package com.example.team2021.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("back")
+public class backController {
+    @GetMapping("index")
+    public String getIndex(){
+        return "index_3_b";
+    }
+}
